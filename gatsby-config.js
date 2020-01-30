2f297cf2274dc3ebb9eb36bfe25ec0a7c58eaa01
Fabio Vedovelli <fabio.vedovelli@raisin.com>
2020-01-30 10:34:43 +0100

    Install and configure Algolia and its dependencies.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,6 @@
 const path = require("path")
+const queries = require("./src/utils/algolia")
+require("dotenv").config()
 
 module.exports = {
   siteMetadata: {
@@ -11,6 +13,15 @@ module.exports = {
     `gatsby-plugin-netlify`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        queries,
+        chunkSize: 10000, // default: 1000
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
